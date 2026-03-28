e9b97ac612e2f164e98acc6256c01881554f747a
chashchina-ks <shipuchkaksusha@gmail.com>
2026-03-28 05:13:10 +0300

    normal_iter.py

--- lab 1/normal_iter.py
+++ lab 1/normal_iter.py
@@ -1,14 +1,18 @@
 import functools
 
-def fib_elem_gen():
-    """Генератор, возвращающий элементы ряда Фибоначчи"""
-    a = 0
-    b = 1
-    while True:
-        yield a
-        res = a + b
-        a = b
-        b = res
+class FibIterator:
+    """Класс-итератор для генерации чисел Фибоначчи"""
+    def __init__(self):
+        self.a = 0
+        self.b = 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        current = self.a
+        self.a, self.b = self.b, self.a + self.b
+        return current
 
 def my_genn():
     """Сопрограмма: принимает число n через send(), возвращает список из первых n чисел Фибоначчи"""
@@ -16,15 +20,17 @@ def my_genn():
         # Получаем количество элементов через send()
         number_of_fib_elem = yield
 
-        # Простая валидация входных данных
+        # Проверка на корректность ввода (опционально, но полезно для тестов)
         if not isinstance(number_of_fib_elem, int) or number_of_fib_elem < 0:
             raise ValueError("Ожидается неотрицательное целое число")
 
-        # Создаем новый генератор для каждого запроса, чтобы начать ряд с начала
-        local_fib = fib_elem_gen()
+        # Создаем новый экземпляр итератора для каждого запроса, чтобы начать с начала ряда
+        fib_iter = FibIterator()
 
-        # Генерируем список, используя встроенную функцию next() для генератора
-        result = [next(local_fib) for _ in range(number_of_fib_elem)]
+        # Генерируем список, используя наш итератор
+        result = []
+        for _ in range(number_of_fib_elem):
+            result.append(next(fib_iter))
 
         yield result
 
@@ -33,16 +39,9 @@ def fib_coroutine(g):
     @functools.wraps(g)
     def inner(*args, **kwargs):
         gen = g(*args, **kwargs)
-        next(gen)  # Запускаем генератор
+        next(gen)  # Запускаем генератор (эквивалент gen.send(None))
         return gen
     return inner
 
 # Применяем декоратор
 my_genn = fib_coroutine(my_genn)
-
-# Пример использования (можно закомментировать при импорте в тестах)
-if __name__ == "__main__":
-    gen = my_genn()
-    print(gen.send(3))   # Ожидается: [0, 1, 1]
-    print(gen.send(5))   # Ожидается: [0, 1, 1, 2, 3]
-    print(gen.send(8))   # Ожидается: [0, 1, 1, 2, 3, 5, 8, 13]
